332786f68839e25a7b05a475286489d1fde49ce9
Damien Zhao <zdm65477730@126.com>
2025-04-30 09:45:01 +0800

    display plugin name from json file and some optimizations
    Signed-off-by: Damien Zhao <zdm65477730@126.com>

--- Makefile
+++ Makefile
@@ -215,16 +215,18 @@ $(BUILD):
 	@[ -d $@ ] || mkdir -p $@
 	@$(MAKE) --no-print-directory -C $(BUILD) -f $(CURDIR)/Makefile
 
-	@rm -rf out/
-	@mkdir -p out/switch/.overlays/
-	@cp $(CURDIR)/$(TARGET).ovl out/switch/.overlays/$(TARGET).ovl
+	@rm -rf $(CURDIR)/SdOut
+	@mkdir -p $(CURDIR)/SdOut/switch/.overlays
+	@mkdir -p $(CURDIR)/SdOut/config/$(APP_TITLE)/lang
+	@mkdir -p $(CURDIR)/SdOut/config/$(APP_TITLE)/themes
+	@cp -r ovlmenu.ovl $(CURDIR)/SdOut/switch/.overlays/
+	@cp -r $(CURDIR)/lang/* $(CURDIR)/SdOut/config/$(APP_TITLE)/lang/
+	@cp -r $(CURDIR)/themes/* $(CURDIR)/SdOut/config/$(APP_TITLE)/themes/
+	@cd $(CURDIR)/SdOut; zip -r -q -9 $(APP_TITLE).zip switch config; cd $(CURDIR)
 
 #---------------------------------------------------------------------------------
 clean:
-	@rm -fr $(BUILD) $(TARGET).ovl $(TARGET).nro $(TARGET).nacp $(TARGET).elf
-
-	@rm -rf out/
-	@rm -f $(TARGET).zip
+	@rm -fr $(BUILD) $(CURDIR)/SdOut $(TARGET).ovl $(TARGET).nro $(TARGET).nacp $(TARGET).elf
 
 #---------------------------------------------------------------------------------
 dist: all
--- source/utils.hpp
+++ source/utils.hpp
@@ -382,6 +382,8 @@ std::string getLocalIpAddress() {
 
     
     // Get the current IP address
+    if (R_FAILED(rc = nifmInitialize(NifmServiceType_User))) // for local IP
+        return UNAVAILABLE_SELECTION;
     rc = nifmGetCurrentIpAddress(&ipAddress);
     if (R_SUCCEEDED(rc)) {
         // Convert the IP address to a string
@@ -519,6 +521,7 @@ const char* getStorageInfo(const std::string& storageType) {
 
 void unpackDeviceInfo() {
     u64 packed_version;
+    splInitialize();
     splGetConfig((SplConfigItem)2, &packed_version);
     memoryType = getMemoryType(packed_version);
     //memoryVendor = UNAVAILABLE_SELECTION;
@@ -540,6 +543,7 @@ void unpackDeviceInfo() {
     formatVersion(packed_version, 24, 16, 8, hosVersion);
 
     splGetConfig((SplConfigItem)65007, &packed_version);
+    splExit();
     usingEmunand = (packed_version != 0);
 
 
@@ -3085,6 +3089,15 @@ void processCommand(const std::vector<std::string>& cmd, const std::string& pack
             std::string destinationPath = cmd[2];
             preprocessPath(destinationPath, packagePath);
             bool downloadSuccess = false;
+            Result rc;
+            if (R_FAILED(rc = socketInitializeDefault())) {
+                return;
+            }
+            if (R_FAILED(rc = nifmInitialize(NifmServiceType_User))) {
+                socketExit();
+                return;
+            }
+            initializeCurl();
             for (size_t i = 0; i < 3; ++i) {
                 downloadSuccess = downloadFile(fileUrl, destinationPath);
                 if (abortDownload.load(std::memory_order_acquire)) {
@@ -3093,6 +3106,9 @@ void processCommand(const std::vector<std::string>& cmd, const std::string& pack
                 }
                 if (downloadSuccess) break;
             }
+            cleanupCurl();
+            nifmExit();
+            socketExit();
             commandSuccess = downloadSuccess && commandSuccess;
         }
     } else if (commandName == "unzip") {
@@ -3178,9 +3194,8 @@ void processCommand(const std::vector<std::string>& cmd, const std::string& pack
             }
         }
         
-        i2cExit();
-        splExit();
-        fsdevUnmountAll();
+        //fsdevUnmountAll();
+        spsmInitialize();
         spsmShutdown(SpsmShutdownMode_Reboot);
         spsmExit();
     } else if (commandName == "shutdown") {
@@ -3191,9 +3206,8 @@ void processCommand(const std::vector<std::string>& cmd, const std::string& pack
                 powerOffAllControllers();
             }
         } else {
-            //spsmInitialize();
-            splExit();
-            fsdevUnmountAll();
+            //fsdevUnmountAll();
+            spsmInitialize();
             spsmShutdown(SpsmShutdownMode_Normal);
             spsmExit();
         }
