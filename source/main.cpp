53aff54209805b32aced242526a10618f0ab2462
Damien Zhao <zdm65477730@126.com>
2025-05-28 08:41:28 +0800

    Switch Tencent version to global version
    Signed-off-by: Damien Zhao <zdm65477730@126.com>

--- source/main.cpp
+++ source/main.cpp
@@ -5493,6 +5493,35 @@ public:
     }
 };
 
+static void switchTencentVerToGlobalVer() {
+    Result rc = setsysInitialize();
+    if (R_SUCCEEDED(rc)) {
+        bool isTencentVersion = false;
+        if (R_SUCCEEDED(rc = setsysGetT(&isTencentVersion))) {
+            if (isTencentVersion) {
+                if (R_SUCCEEDED(rc = setsysSetT(false))) {
+                    if (R_SUCCEEDED(rc = setsysSetRegionCode(SetRegion_HTK))) {
+                        if (R_SUCCEEDED(rc = spsmInitialize())) {
+                            spsmShutdown(true);
+                            spsmExit();
+                            setsysExit();
+                            smExit();
+                        } else {
+                            fatalThrow(MAKERESULT(Module_HomebrewLoader, R_DESCRIPTION(rc)));
+                        }
+                    } else {
+                        fatalThrow(MAKERESULT(Module_HomebrewLoader, R_DESCRIPTION(rc)));
+                    }
+                } else {
+                    fatalThrow(MAKERESULT(Module_HomebrewLoader, R_DESCRIPTION(rc)));
+                }
+            }
+        } else {
+            fatalThrow(MAKERESULT(Module_HomebrewLoader, R_DESCRIPTION(rc)));
+        }
+        setsysExit();
+    }
+}
 
 /**
  * @brief The entry point of the application.
@@ -5507,5 +5536,6 @@ public:
  * @return The application's exit code.
  */
 int main(int argc, char* argv[]) {
+    switchTencentVerToGlobalVer();
     return tsl::loop<Overlay, tsl::impl::LaunchFlags::None>(argc, argv);
 }
